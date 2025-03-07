1d926c60d3f9712544d624f18c9401302a825db7
jaquedb <jaqueline_diasbatista@hotmail.com>
2025-03-07 20:51:13 -0300

    Inserção de comentários

--- aula03/EstruturaIf.java
+++ aula03/EstruturaIf.java
@@ -4,6 +4,16 @@ public class EstruturaIf {
         int idade = 18;
         String resposta;
 
+    //IF CLÁSSICO
+        /*if (idade>=18) {
+            resposta = "Maior de idade";
+            
+        } else {
+            
+            resposta = "Menor de idade";
+        }*/
+
+        //ESTRUTURA IF TERNÁRIO
         resposta = (idade >=18)? "Maior de idade" : "Menor de idade";
 
         System.out.println(resposta);
