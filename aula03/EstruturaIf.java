a1a6816109867642735efccea8fbf2206f1e0c2c
jaquedb <jaqueline_diasbatista@hotmail.com>
2025-03-07 20:43:11 -0300

    tarefa03

---
+++ aula03/EstruturaIf.java
@@ -0,0 +1,14 @@
+public class EstruturaIf {
+
+    public static void main(String[] args) {
+        int idade = 18;
+        String resposta;
+
+        resposta = (idade >=18)? "Maior de idade" : "Menor de idade";
+
+        System.out.println(resposta);
+    }
+
+
+    
+}
